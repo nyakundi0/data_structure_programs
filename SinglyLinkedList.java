784a6c519fb24bbf05c7c00887250e18515032b4
nyakundi0 <nyakundiedwine0@gmail.com>
2024-10-11 13:59:30 +0300

    singly linked list

---
+++ SinglyLinkedList.java
@@ -0,0 +1,11 @@
+public class  SinglyLinkedList {
+    class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+
+        }
+    }
+}
