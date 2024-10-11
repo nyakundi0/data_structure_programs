96b7d7d1c39b658c9e5fa0761d7607458b2f2c5a
nyakundi0 <nyakundiedwine0@gmail.com>
2024-10-11 12:44:27 +0300

    added prompt

--- Array.java
+++ Array.java
@@ -1,15 +1,17 @@
 import java.util.*;
-public class Array{
-    public static void main(String[] args) {
-        int [] numbers=new int[5];
-        Scanner in=new Scanner (System.in);
-        for(int i=0;i<numbers.length;i++){
-            numbers[i]=in.nextInt();
-            for (i=0;i<numbers.length;i++){
-                System.out.println(numbers[i]+" ");
+public class Array {
 
+    public static void main(String[] args) {
+        int [] numbers=new int[7];
+        Scanner in=new Scanner(System.in);
+        for(int i=0;i<numbers.length;i++){
+            System.out.println("please enetr 7 num bers below to store in ana array");
+            numbers[i]=in.nextInt();
+            System.out.println("the numbers in the array are:");
+            for(i=0;i<numbers.length;i++){
+                System.out.println(numbers[i]);
             }
+
         }
     }
-
 }
